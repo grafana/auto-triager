6857daaa7c604ac8b1d1598bea9236550ac785e0
Esteban Beltran <esteban@academo.me>
2024-08-21 16:48:47 +0200

    Remove output if debug is not 1

--- pkg/cmd/triager-fine-tuned/triager-fine-tuned.go
+++ pkg/cmd/triager-fine-tuned/triager-fine-tuned.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -16,6 +17,8 @@ import (
 	"github.com/sashabaranov/go-openai"
 	"github.com/tiktoken-go/tokenizer"
 )
+
+var Debug *log.Logger
 
 type QualityVeredict struct {
 	IsCategorizable bool        `json:"isCategorizable"`
@@ -42,14 +45,14 @@ var (
 		"ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:auto-triage:9ss5MNR0", // 400k training tokens
 		"Model to use",
 	)
-	qualitizerModel = flag.String(
-		"qualitizerModel",
-		// "ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tDkW7Kq", // first training. Bad model
-		// "gpt-4o", // regular model from openai, produces remarks but it not fine tuned
-		// "ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tW2EBhH", // good. trained with a larger dataset but doesn't produce remarks
-		"ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tWTKBHh", // good. trained with a larger dataset. might produce remarks but most of the time it doesn't
-		"Model to use",
-	)
+	// qualitizerModel = flag.String(
+	// 	"qualitizerModel",
+	// 	// "ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tDkW7Kq", // first training. Bad model
+	// 	// "gpt-4o", // regular model from openai, produces remarks but it not fine tuned
+	// 	// "ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tW2EBhH", // good. trained with a larger dataset but doesn't produce remarks
+	// 	"ft:gpt-4o-mini-2024-07-18:grafana-labs-experiments-exploration:issue-qualitizer:9tWTKBHh", // good. trained with a larger dataset. might produce remarks but most of the time it doesn't
+	// 	"Model to use",
+	// )
 	labelsFile = flag.String(
 		"labelsFile",
 		"fixtures/areaLabels.txt",
@@ -65,35 +68,40 @@ var (
 func main() {
 	flag.Parse()
 
+	if os.Getenv("DEBUG") == "1" {
+		Debug = log.New(os.Stdout, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		Debug = log.New(io.Discard, "", 0)
+	}
+
 	err := validateFlags()
 	if err != nil {
-		fmt.Printf("Error validating flags: %v\n", err)
+		Debug.Fatalf("Error validating flags: %v\n", err)
 		os.Exit(1)
 	}
 
 	areaLabels, err := readFileLines(*labelsFile)
 	if err != nil {
-		log.Fatal("Error reading areaLabels.txt: ", err)
+		Debug.Fatalf("Error reading areaLabels.txt: %v\n", err)
 	}
 
 	typeLabels, err := readFileLines(*typesFile)
 	if err != nil {
-		log.Fatal("Error reading typeLabels.txt: ", err)
+		Debug.Fatalf("Error reading typeLabels.txt: %v\n", err)
 	}
 
 	issueData, err := github.FetchIssueDetails(*issueId, *repo)
 	if err != nil {
-		log.Fatal("Error fetching issue details: ", err)
+		Debug.Fatalf("Error fetching issue details: %v\n", err)
 	}
 
 	if issueData.Title == "" {
-		log.Fatal("Error fetching issue details: Title is empty")
+		Debug.Fatal("Error fetching issue details: Title is empty")
 		os.Exit(1)
 	}
 
-	fmt.Printf(":: Got issue %d with title: %s\n", *issueId, issueData.Title)
-
-	fmt.Printf(":: Checking if issue can be categorized\n")
+	Debug.Printf(":: Got issue %d with title: %s\n", *issueId, issueData.Title)
+	// Debug.Printf(":: Checking if issue can be categorized\n")
 
 	// qualityVeredict, err := getIssueIsCategorizable(&issueData, qualitizerModel)
 	//
@@ -102,17 +110,16 @@ func main() {
 	// 	os.Exit(1)
 	// }
 	//
-	// fmt.Printf("Is categorizable: %s\n", strconv.FormatBool(qualityVeredict.IsCategorizable))
+	// Debug.Printf("Is categorizable: %s\n", strconv.FormatBool(qualityVeredict.IsCategorizable))
 
-	fmt.Printf(":: Categorizing issue\n")
+	Debug.Printf(":: Categorizing issue\n")
 
 	category, err := getIssueCategory(&issueData, categorizerModel, typeLabels, areaLabels)
 	if err != nil {
-		log.Fatal("Error categorizing issue: ", err)
-		os.Exit(1)
+		Debug.Fatalf("Error categorizing issue: %v\n", err)
 	}
 
-	fmt.Printf("Categorizaion: %v\n", category)
+	fmt.Printf("%v", category)
 
 }
 
@@ -181,7 +188,7 @@ func getIssueCategory(
 		return CategorizedIssue{}, err
 	}
 
-	fmt.Printf("Tokens: %d\n", len(tokens))
+	Debug.Printf("Tokens: %d\n", len(tokens))
 
 	client := openai.NewClient(openAiKey)
 	resp, err := client.CreateChatCompletion(
@@ -209,7 +216,7 @@ func getIssueCategory(
 	)
 
 	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
+		Debug.Printf("ChatCompletion error: %v\n", err)
 		return CategorizedIssue{}, err
 	}
 
@@ -223,45 +230,44 @@ func getIssueCategory(
 
 }
 
-func getIssueIsCategorizable(issueData *github.Issue, model *string) (QualityVeredict, error) {
-	client := openai.NewClient(openAiKey)
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model: *model,
-			ResponseFormat: &openai.ChatCompletionResponseFormat{
-				Type: openai.ChatCompletionResponseFormatTypeJSONObject,
-			},
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: prompts.QualitySystemPrompt,
-				},
-				{
-					Role: openai.ChatMessageRoleUser,
-					Content: `
-					  Issue ID: ` + strconv.Itoa(issueData.Number) + `
-					  Issue title: ` + issueData.Title + `
-					  Issue description:\n\n ` + issueData.Body + `
-					`,
-				},
-			},
-		},
-	)
-
-	if err != nil {
-		fmt.Printf("ChatCompletion error: %v\n", err)
-		return QualityVeredict{}, err
-	}
-
-	fmt.Println(resp.Choices[0].Message.Content)
-
-	veredict := QualityVeredict{}
-	err = json.Unmarshal([]byte(resp.Choices[0].Message.Content), &veredict)
-	if err != nil {
-		return QualityVeredict{}, err
-	}
-
-	return veredict, nil
-
-}
+// func getIssueIsCategorizable(issueData *github.Issue, model *string) (QualityVeredict, error) {
+// 	client := openai.NewClient(openAiKey)
+// 	resp, err := client.CreateChatCompletion(
+// 		context.Background(),
+// 		openai.ChatCompletionRequest{
+// 			Model: *model,
+// 			ResponseFormat: &openai.ChatCompletionResponseFormat{
+// 				Type: openai.ChatCompletionResponseFormatTypeJSONObject,
+// 			},
+// 			Messages: []openai.ChatCompletionMessage{
+// 				{
+// 					Role:    openai.ChatMessageRoleSystem,
+// 					Content: prompts.QualitySystemPrompt,
+// 				},
+// 				{
+// 					Role: openai.ChatMessageRoleUser,
+// 					Content: `
+// 					  Issue ID: ` + strconv.Itoa(issueData.Number) + `
+// 					  Issue title: ` + issueData.Title + `
+// 					  Issue description:\n\n ` + issueData.Body + `
+// 					`,
+// 				},
+// 			},
+// 		},
+// 	)
+// 	if err != nil {
+// 		Debug.Printf("ChatCompletion error: %v\n", err)
+// 		return QualityVeredict{}, err
+// 	}
+//
+// 	Debug.Println(resp.Choices[0].Message.Content)
+//
+// 	veredict := QualityVeredict{}
+// 	err = json.Unmarshal([]byte(resp.Choices[0].Message.Content), &veredict)
+// 	if err != nil {
+// 		return QualityVeredict{}, err
+// 	}
+//
+// 	return veredict, nil
+//
+// }
